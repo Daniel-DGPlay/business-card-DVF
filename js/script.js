2c2ce34fd199317523a1cda1f7da95ecd086aa41
DGPlay Code <99695391+Daniel-DGPlay@users.noreply.github.com>
2024-01-11 14:42:34 -0300

    Atualizar o script.js - Adicionado a função de atualização de idade automática.
    - Adicionado a função para calcular a idade baseado na data de nascimento e a data atual.
    - Adicionado a função para atualizar a idade na página.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ for (var i = 0; i < btns.length; i++) {
     this.style.setProperty("--eixoY", y + "px");
   };
 }
+
+  // Função para calcular a idade
+  function calcularIdade(dataNascimento) {
+    const dataNasc = new Date(dataNascimento);
+    const dataAtual = new Date();
+
+    const diff = Math.abs(dataAtual - dataNasc);
+    const idade = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+
+    return idade;
+  }
+
+  // Atualiza a idade na página
+  document.getElementById("age").innerText = calcularIdade("1997-01-12");
